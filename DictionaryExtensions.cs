39b9488781799dad346a819f18a4361c0f267b62
Gerardo Marset <gammer1994@gmail.com>
2022-06-04 18:20:30 -0300

    Add Deconstruct for dictionaries

---
+++ DictionaryExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Ideka.BHUDCommon
+{
+    internal static class DictionaryExtensions
+    {
+        public static void Deconstruct<TK, TV>(this KeyValuePair<TK, TV> pair, out TK key, out TV value)
+        {
+            key = pair.Key;
+            value = pair.Value;
+        }
+    }
+}
