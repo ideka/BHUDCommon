0eaee1928b3dd22bfbb82a788d7930c66286540a
Gerardo Marset <gammer1994@gmail.com>
2022-06-05 21:50:22 -0300

    Add MaxBy and MinBy

---
+++ EnumerableExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ideka.BHUDCommon
+{
+    internal static class EnumerableExtensions
+    {
+        public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+            => source.MaxBy(selector, null);
+
+        public static TSource MaxBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector,
+            IComparer<TKey> comparer)
+            => source.MostBy(selector, comparer, true);
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector)
+            => source.MinBy(selector, null);
+
+        public static TSource MinBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> selector,
+            IComparer<TKey> comparer)
+            => source.MostBy(selector, comparer, false);
+
+        private static TSource MostBy<TSource, TKey>(this IEnumerable<TSource> source,
+            Func<TSource, TKey> selector, IComparer<TKey> comparer, bool max)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (selector == null) throw new ArgumentNullException(nameof(selector));
+            comparer = comparer ?? Comparer<TKey>.Default;
+            int factor = max ? -1 : 1;
+
+            using (var sourceIterator = source.GetEnumerator())
+            {
+                if (!sourceIterator.MoveNext())
+                    throw new InvalidOperationException("Sequence contains no elements");
+
+                var most = sourceIterator.Current;
+                var mostKey = selector(most);
+                while (sourceIterator.MoveNext())
+                {
+                    var candidate = sourceIterator.Current;
+                    var candidateProjected = selector(candidate);
+                    if (comparer.Compare(candidateProjected, mostKey) * factor < 0)
+                    {
+                        most = candidate;
+                        mostKey = candidateProjected;
+                    }
+                }
+
+                return most;
+            }
+        }
+    }
+}
