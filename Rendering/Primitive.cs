531f3e2e460a24e96ba64b548d730934e0863ef9
Gerardo Marset <gammer1994@gmail.com>
2022-06-07 01:51:56 -0300

    Rework Primitive to maintain depth information

--- Rendering/Primitive.cs
+++ Rendering/Primitive.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using static Blish_HUD.GameService;
@@ -9,6 +8,39 @@ namespace Ideka.BHUDCommon
     public class Primitive
     {
         public List<Vector3> Points { get; }
+
+        public class ScreenPrimitive
+        {
+            public List<Vector2> Points { get; } = new List<Vector2>();
+            public float Depth { get; }
+            private float MaxDepth => Gw2Mumble.PlayerCamera.FarPlaneRenderDistance;
+
+            public ScreenPrimitive(IEnumerable<Vector3> points)
+            {
+                float sum = 0;
+                int count = 0;
+                foreach (var point in points)
+                {
+                    // Don't draw stuff "behind the camera"
+                    if (point.Z < 0)
+                    {
+                        Points.Clear();
+                        return;
+                    }
+
+                    Points.Add(Flatten(point));
+                    sum += point.Z;
+                    count++;
+                }
+
+                Depth = sum / count / MaxDepth;
+            }
+
+            public static Vector2 Flatten(Vector3 v)
+                => new Vector2(
+                    (v.X / v.Z + 1) / 2 * Graphics.SpriteScreen.Width,
+                    (1 - v.Y / v.Z) / 2 * Graphics.SpriteScreen.Height);
+        }
 
         public Primitive(IEnumerable<Vector3> points)
         {
@@ -28,24 +60,14 @@ namespace Ideka.BHUDCommon
         public Primitive Transformed(Matrix matrix)
             => new Primitive(Transform(matrix));
 
-        public IEnumerable<Vector2> ToScreen()
-        {
-            foreach (var p in Points)
-                yield return ToScreen(p);
-        }
+        public ScreenPrimitive ToScreen()
+            => new ScreenPrimitive(Points.Select(ToScreen));
 
         public static Primitive operator +(Primitive a, Primitive b)
             => new Primitive(a.Points.Concat(b.Points));
 
-        public static Vector2 Flatten(Vector3 v)
-            => v.Z <= 0
-                ? new Vector2(float.NaN, float.NaN)  // TODO: Figure out a more graceful way of handling this edge case
-                : new Vector2(
-                    (v.X / v.Z + 1) / 2 * Graphics.SpriteScreen.Width,
-                    (1 - v.Y / v.Z) / 2 * Graphics.SpriteScreen.Height);
-
-        public static Vector2 ToScreen(Vector3 point)
-            => Flatten(Vector3.Transform(point, Gw2Mumble.PlayerCamera.WorldViewProjection));
+        public static Vector3 ToScreen(Vector3 point)
+            => Vector3.Transform(point, Gw2Mumble.PlayerCamera.WorldViewProjection);
 
         public static Primitive HorizontalCircle(float radius, int sides)
         {
