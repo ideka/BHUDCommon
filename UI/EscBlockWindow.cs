0cd4bac32230959cde101cd36bb9126d927851f4
Gerardo Marset <gammer1994@gmail.com>
2023-01-18 03:05:29 -0300

    Fix EscBlockWindow to work with BlishHUD 1.0

--- UI/EscBlockWindow.cs
+++ UI/EscBlockWindow.cs
@@ -1,17 +1,22 @@
 ﻿using Blish_HUD.Controls;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace Ideka.BHUDCommon
 {
     public class EscBlockWindow : WindowBase2
     {
-        public EscBlockWindow(Container parent)
+        private readonly Container _owner;
+
+        public EscBlockWindow(Container owner)
         {
             TopMost = true;
             CanClose = false;
             Visible = false;
-            Parent = parent;
+            Parent = Graphics.SpriteScreen;
+            _owner = owner;
+            _owner.Disposed += OwnerDisposed;
         }
 
         public void BlockStart()
@@ -23,9 +28,18 @@ namespace Ideka.BHUDCommon
         public void BlockEnd()
         {
             Visible = false;
-            FocusedControl = Parent;
+            FocusedControl = _owner;
         }
 
         public override void Draw(SpriteBatch spriteBatch, Rectangle drawBounds, Rectangle scissor) { }
+
+        private void OwnerDisposed(object sender, EventArgs e) => Dispose();
+
+        protected override void DisposeControl()
+        {
+            _owner.Disposed -= OwnerDisposed;
+
+            base.DisposeControl();
+        }
     }
 }
