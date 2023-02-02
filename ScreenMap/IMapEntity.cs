751ccb6593c4b31afe7bd770d636868a089166c2
Gerardo Marset <gammer1994@gmail.com>
2023-02-02 15:40:50 -0300

    Add MapData and ScreenMap

---
+++ ScreenMap/IMapEntity.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace Ideka.BHUDCommon
+{
+    public interface IMapEntity
+    {
+        void DrawToMap(SpriteBatch spriteBatch, IMapBounds map);
+    }
+}
