6c604309a657c652e2904cea6ba9e8ba2373831f
Gerardo Marset <gammer1994@gmail.com>
2023-02-07 12:27:06 -0300

    Add binding events to KeyBindingSetting

--- Settings/KeyBindingSetting.cs
+++ Settings/KeyBindingSetting.cs
@@ -9,7 +9,8 @@ namespace Ideka.BHUDCommon;
 
 public class KeyBindingSetting : GenericSetting<KeyBinding>
 {
-    private Action _action;
+    private Action _activated;
+    private Action<KeyBinding> _bindingChanged;
 
     public KeyBindingSetting(SettingCollection settings,
         string key, KeyBinding defaultValue, Func<string> displayNameFunc, Func<string> descriptionFunc)
@@ -17,15 +18,30 @@ public class KeyBindingSetting : GenericSetting<KeyBinding>
         Initialize(settings.DefineSetting(key, defaultValue, displayNameFunc, descriptionFunc));
         Setting.Value.Enabled = true;
         Setting.Value.Activated += Activated;
+        Setting.Value.BindingChanged += BindingChanged;
     }
 
     public IDisposable OnActivated(Action action)
     {
-        _action += action;
-        return new WhenDisposed(() => _action -= action);
+        _activated += action;
+        return new WhenDisposed(() => _activated -= action);
     }
 
-    private void Activated(object sender, EventArgs e) => _action?.Invoke();
+    public IDisposable OnBindingChanged(Action<KeyBinding> action)
+    {
+        _bindingChanged += action;
+        return new WhenDisposed(() => _bindingChanged -= action);
+    }
+
+    public IDisposable OnBindingChangedAndNow(Action<KeyBinding> action)
+    {
+        action(Value);
+        return OnBindingChanged(action);
+    }
+
+    private void Activated(object sender, EventArgs e) => _activated?.Invoke();
+
+    private void BindingChanged(object sender, EventArgs e) => _bindingChanged?.Invoke(Value);
 
     public override void Dispose()
     {
