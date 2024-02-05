6c551a24d22c91120d0b4f783b8781eefd874cfc
Gerardo Marset <gammer1994@gmail.com>
2024-02-05 21:52:25 +0000

    Add Vector3Utils

---
+++ Vector3Utils.cs
@@ -0,0 +1,20 @@
+﻿using Ideka.NetCommon;
+using Microsoft.Xna.Framework;
+using System;
+
+namespace Ideka.BHUDCommon;
+
+public static class Vector3Utils
+{
+    public static int DistanceSign(Vector3 a, Vector3 b, float distance)
+        => Math.Sign((a - b).LengthSquared() - MathUtils.Squared(distance));
+
+    public static bool DistanceUnder(Vector3 a, Vector3 b, float distance)
+        => DistanceSign(a, b, distance) < 0;
+
+    public static bool DistanceOver(Vector3 a, Vector3 b, float distance)
+        => DistanceSign(a, b, distance) > 0;
+
+    public static bool DistanceEqual(Vector3 a, Vector3 b, float distance)
+        => DistanceSign(a, b, distance) == 0;
+}
