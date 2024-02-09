f699f7c88424821e6e5bb3a9d1b170604a543f98
Gerardo Marset <gammer1994@gmail.com>
2024-02-09 18:13:04 +0000

    Add SkillData

---
+++ ApiCache/SkillData.cs
@@ -0,0 +1,13 @@
+﻿using Gw2Sharp.WebApi.V2.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static Blish_HUD.GameService;
+
+namespace Ideka.BHUDCommon;
+
+public class SkillData : ApiCache<int, Skill>
+{
+    protected override async Task<IEnumerable<Skill>> ApiGetter(CancellationToken ct)
+        => await Gw2WebApi.AnonymousConnection.Client.V2.Skills.AllAsync(ct);
+}
