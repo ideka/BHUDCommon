72502ee2c50a12a5b7befe762edec08e6473edbc
Gerardo Marset <gammer1994@gmail.com>
2024-02-11 09:06:14 +0000

    Add TimeSpanConverter

---
+++ JsonConverters/TimeSpanConverter.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Ideka.BHUDCommon;
+
+public class TimeSpanConverter : JsonConverter<TimeSpan>
+{
+    public override TimeSpan ReadJson(JsonReader reader, Type objectType, TimeSpan existingValue, bool hasExistingValue, JsonSerializer serializer)
+        => TimeSpan.FromTicks(serializer.Deserialize<long>(reader));
+
+    public override void WriteJson(JsonWriter writer, TimeSpan value, JsonSerializer serializer)
+        => serializer.Serialize(writer, value.Ticks);
+}
