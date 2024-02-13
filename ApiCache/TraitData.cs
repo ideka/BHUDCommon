0ca7fe947bb71d4a01cef554cb59d64a23e9b3bc
Gerardo Marset <gammer1994@gmail.com>
2024-02-13 11:17:36 +0000

    Add TraitData

---
+++ ApiCache/TraitData.cs
@@ -0,0 +1,13 @@
+﻿using Gw2Sharp.WebApi.V2.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static Blish_HUD.GameService;
+
+namespace Ideka.BHUDCommon;
+
+public class TraitData : ApiCache<int, Trait>
+{
+    protected override async Task<IEnumerable<Trait>> ApiGetter(CancellationToken ct)
+        => await Gw2WebApi.AnonymousConnection.Client.V2.Traits.AllAsync(ct);
+}
