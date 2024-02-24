79d547ebdac1760831359881acb700fd5aa3b169
Gerardo Marset <gammer1994@gmail.com>
2024-02-24 06:24:05 +0000

    Fix ApiEnumConverter failing on nullables

--- JsonConverters/ApiEnumConverter.cs
+++ JsonConverters/ApiEnumConverter.cs
@@ -7,9 +7,7 @@ namespace Ideka.BHUDCommon;
 public class ApiEnumConverter : JsonConverter
 {
     public override bool CanConvert(Type objectType)
-    {
-        return objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(ApiEnum<>);
-    }
+        => objectType.IsGenericType && objectType.GetGenericTypeDefinition() == typeof(ApiEnum<>);
 
     public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
     {
@@ -20,15 +18,19 @@ public class ApiEnumConverter : JsonConverter
         }
 
         var valueProperty = value.GetType().GetProperty("Value");
-        var actualValue = valueProperty.GetValue(value);
+        object? actualValue = valueProperty.GetValue(value);
         serializer.Serialize(writer, actualValue);
     }
 
-    public override object ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
+    public override object? ReadJson(JsonReader reader, Type objectType, object? existingValue, JsonSerializer serializer)
     {
+        if (reader.Value == null)
+            return null;
+
         var valueType = objectType.GetGenericArguments()[0];
 
-        var actualValue = serializer.Deserialize(reader, valueType);
+        object? actualValue = serializer.Deserialize(reader, valueType);
+
         var constructorInfo = objectType.GetConstructor([valueType])
             ?? throw new JsonSerializationException($"No matching constructor found for type {objectType}");
 
