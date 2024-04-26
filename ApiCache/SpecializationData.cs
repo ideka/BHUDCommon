a5ee524fa70780e400c69dd68e69e828feca56e9
Gerardo Marset <gammer1994@gmail.com>
2024-04-26 21:15:08 +0100

    Add SpecializationData

---
+++ ApiCache/SpecializationData.cs
@@ -0,0 +1,13 @@
+﻿using Gw2Sharp.WebApi.V2.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using static Blish_HUD.GameService;
+
+namespace Ideka.BHUDCommon;
+
+public class SpecializationData : ApiCache<int, Specialization>
+{
+    protected override async Task<IEnumerable<Specialization>> ApiGetter(CancellationToken ct)
+        => await Gw2WebApi.AnonymousConnection.Client.V2.Specializations.AllAsync(ct);
+}
